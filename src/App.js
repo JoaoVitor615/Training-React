a76becebb11a84727cecdf3b05358121fc7a8fad
João Vítor <jvpeniza@gmail.com>
2023-07-02 20:13:03 -0300

    Adicionando components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
+import List from './components/List';
+
 
 function App() {
+
+  const nome = "Maria"
+
   return (
     <div className="App">
-     <h1>Olá React</h1>
+      <h1>Testando CSS</h1>
+      <Frase/>
+      <SayMyName nome="Matheus"/>
+      <SayMyName nome="João"/>
+      <SayMyName nome={nome}/>
+      <Pessoa 
+       nome="João" 
+       idade="28" 
+       profissao="programador" 
+       foto="https://via.placeholder.com/150"/>
+       <List/>
 
-     <p>Meu primeiro app</p>
     </div>
   );
 }
