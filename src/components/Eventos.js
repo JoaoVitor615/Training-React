e9cdffe0f234a21ef7e561f10954546fc1f13ca8
João Vítor <jvpeniza@gmail.com>
2023-07-03 12:13:37 -0300

    Avançando em props

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,13 +1,20 @@
+import Button from "./evento/Button"
+
 function Eventos() {
 
     function meuEvento(){
         console.log("Ativado")
     }
 
+    function segundoEvento() {
+        console.log('Ativando o segundo evento')
+    }
+
     return(
         <div>
             <p>Clioque aqui para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+            <Button event={meuEvento} text="Primeiro Evento" />
+            <Button event={segundoEvento} text="Segundo Evento" />
         </div>
     )
 }
